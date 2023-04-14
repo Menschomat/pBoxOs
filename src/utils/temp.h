553584d12a5ec126c9df67c8e3fc5329d5058eaf
Falco Weihrauch <falco@weihrauch.me>
2023-04-14 18:36:29 +0200

    added support for raspi pico

--- src/utils/temp.h
+++ src/utils/temp.h
@@ -3,7 +3,7 @@
 #include <DallasTemperature.h>
 #include <utils/mqtt.h>
 
-#define ONE_WIRE_BUS 7 // Sensor DS18B20 am digitalen Pin 2
+#define ONE_WIRE_BUS 13 // Sensor DS18B20 am digitalen Pin 2
 String senors_topic = root_topic + String("") + BOX_ID + "/sensors/";
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
@@ -12,8 +12,10 @@ float lastTemp = -1024;
 
 void initTempSens()
 {
+    Serial.println("BEFORE ONEWIRE_INIT");
     sensors.begin();                      // Starten der Kommunikation mit dem Sensor
     sensorCount = sensors.getDS18Count(); // Lesen der Anzahl der angeschlossenen
+    Serial.println(sensorCount);
 }
 
 float getTemp()
